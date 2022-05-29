943af8666dd66f2e671f767fad065e422cf90742
labexams <86876820+labexams@users.noreply.github.com>
2022-05-29 23:01:41 +0530

    Create check_even_number.c

---
+++ check_even_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    char in[100],f='A';
+    printf("Enter string:");
+    gets(in);
+    for(int i=0;in[i]!='\0';i++){
+        switch (f) {
+            case 'A':f=in[i]=='0'?'B':'A';
+                break;
+            case 'B':f=in[i]=='0'?'B':'A';
+        }
+    }
+    if(f=='B')printf("\nAccepted");
+    else printf("\nRejected");
+}
